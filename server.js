019a29bee84fcb36430a983d2dd4747d79acfdec
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2020-12-20 00:20:05 -0800

    Add dependencies

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+
